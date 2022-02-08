dd5d21a159c9b1a49a90bf057739294293d29a86
Sean R Anderson <sean.hearing@gmail.com>
2022-02-08 09:56:14 -0600

    Update StartHere.R

--- StartHere.R
+++ StartHere.R
@@ -3,34 +3,47 @@
 # replicate particular analyses in case the user is not 
 # familiar with R. For additional information and code, see 
 # Supplementary Appendix 2 accompanying Anderson et al. 
-# (2021).
+# (2022).
 # 
 # Sean R. Anderson - sean.hearing@gmail.com
 
-# Columns in dataset correspond to the target angle, followed 
-# by an underscore, and 'mean' or 'sd' for standard deviation.
-# Rows in dataset correspond to listeners arranged according 
-# to the alphabet.
-dat <- read.table('Anderson2021_PLOSONE_Dataset.txt', 
-                  sep = ',',
-                  header=T)
+# Dataset from accompanying open-access publication supporting files
+  # doi: xx
+  # Took approximately 1 hour to complete author's laptop computer
+raw.dat <- read.csv('S3_Dataset.csv',head=T)
 
+# Load in dependencies
+library(reshape2)
 
-raw.dat <- read.table('Anderson2021_PLOSONE_forRegression.txt',
-                      sep = ',',
-                      header = T)
+# Aggregate data by mean and SD
+agg.mn <- aggregate(RespAz ~ SubID + TargAz, 
+                    data = raw.dat, 
+                    FUN = mean)
+agg.sd <- aggregate(RespAz ~ SubID + TargAz, 
+                    data = raw.dat, 
+                    FUN = sd)
+agg <- rbind(agg.mn, agg.sd)
+agg$mnvssd <- c(rep(1, length( agg[,1]) / 2), # mean
+                rep(2, length( agg[,1]) / 2)) # SD
+agg <- with(agg,
+            agg[order(SubID, TargAz, mnvssd), ])
+
+# Cast data into new dataframe
+dat <- acast(agg, SubID ~ TargAz + mnvssd, 
+             value.var = 'RespAz')
+dat <- dat[, c(seq(1,37,2), seq(2,38,2)) ]
 
 # ------------------- INSTRUCTIONS FOR USE -------------------
 # - Run `HelperFunctions.R` and `LocalizationCategoryDefinitions.R` 
 #   to input functions to the workspace.
 #
 # - Run `SimulationStudy.R` script to replicate the 
-#   simulation study in Anderson et al. (2021).
+#   simulation study in Anderson et al. (2022).
 #     - Note: This will take a long time to complete (e.g., one 
-#       one day on the primary author's computer).
+#       one day on the primary author's laptop computer).
 #
 # - Run `LocalizationCategories.R` script to assign listeners 
 #   to categories.
 # 
 # - Run `MiscRegressionAnalyses.R` to replicate the regression 
-#   results from Anderson et al. (2021).
+#   results from Anderson et al. (2022).
